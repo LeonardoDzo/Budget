f56aa6ebc3395a63c92df2c899e3266d506b229e
Leonardo <leo.durazod@gmail.com>
2021-02-07 11:30:50 -0700

    Porjext created

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Leonardo Durazo on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
